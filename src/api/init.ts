6cb8ca03924e0cc183180dc166a2187dbb01ea3a
Eric Sexton <foopis23@gmail.com>
2023-04-08 12:36:12 -0400

    swagger

--- src/api/init.ts
+++ src/api/init.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import { Tonelist } from '../tonelist';
 import v1Routes from './v1';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 
 async function initAPI(tonelist: Tonelist) {
 	const logger = tonelist.logger.child({ module: 'api' });
@@ -8,7 +10,37 @@ async function initAPI(tonelist: Tonelist) {
 		logger: logger,
 	});
 
-	fastify.register(v1Routes, { prefix: '/api/v1', tonelist },)
+	await fastify.register(fastifySwagger, {
+		swagger: {
+			info: {
+				version: '1.0.0',
+				title: 'Tonelist2 API',
+				description: 'Control Tonelist2 from an API'
+			},
+			host: 'localhost:3000',
+			schemes: ['http'],
+			consumes: ['application/json'],
+			produces: ['application/json'],
+			securityDefinitions: {
+				ApiKey: {
+					type: 'apiKey',
+					name: 'X-API-KEY',
+					in: 'header'
+				},
+				// BearerAuth: {
+				// 	type: 'http',
+				// 	scheme: 'bearer',
+				// 	bearerFormat: 'JWT'
+				// }
+			}
+		}
+	});
+
+	await fastify.register(fastifySwaggerUI, {
+		routePrefix: '/api/documentation'
+	});
+
+	fastify.register(v1Routes, { prefix: '/api/v1', tonelist });
 
 	try {
 		await fastify.listen({ port: 3000, host: '0.0.0.0' });

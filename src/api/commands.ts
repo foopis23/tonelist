668afe3987667fdc2309f954276164b1546724e0
Eric Sexton <foopis23@gmail.com>
2023-04-20 00:22:12 -0400

    added command schema

--- src/api/commands.ts
+++ src/api/commands.ts
@@ -60,7 +60,48 @@ function buildSchema(command: CommandConfig) {
 			{
 				apiKey: []
 			}
-		]
+		],
+		response: {
+			200: {
+				type: 'object',
+				properties: {
+					message: { type: 'string' },
+				},
+				additionalProperties: true
+			},
+			400: {
+				type: 'object',
+				properties: {
+					statusCode: { type: 'number' },
+					error: { type: 'string' },
+					message: { type: 'string' }
+				}
+			},
+			401: {
+				type: 'object',
+				properties: {
+					statusCode: { type: 'number' },
+					error: { type: 'string' },
+					message: { type: 'string' }
+				}
+			},
+			403: {
+				type: 'object',
+				properties: {
+					statusCode: { type: 'number' },
+					error: { type: 'string' },
+					message: { type: 'string' }
+				}
+			},
+			500: {
+				type: 'object',
+				properties: {
+					statusCode: { type: 'number' },
+					error: { type: 'string' },
+					message: { type: 'string' }
+				}
+			}
+		}
 	};
 
 	return schema;
